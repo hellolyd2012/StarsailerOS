f6e460e062c766c561f320d087077e6b966f3eb2
lyd <lyd20122024@outlook.com>
2024-11-01 18:21:34 +0800

    MultiBoot规范

---
+++ Includes/Arch/X86/Boot/MultiBoot.h
@@ -0,0 +1,12 @@
+// 创建于2024.11.1 
+// StarsailerOS X86 Boot MultiBoot文件
+// 代码遵循GNU GPLV2发布
+
+#ifndef StarsailerOS_MultiBoot_H
+#define StarsailerOS_MultiBoot_H
+
+#define MAGIC 0x1BADB002 // MultiBoot MAGIC 魔数
+#define ALI 0x03 // 对齐
+#define CHECKSUM(Flags) -(MAGIC + Flags) // 校验和
+
+#endif // StarsailerOS_MultiBoot_H
